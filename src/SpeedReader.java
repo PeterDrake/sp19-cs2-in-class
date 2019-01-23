678f53490629ee8a4d9e84b0dee7a2aaa651bbe7
Peter Drake <drake@lclark.edu>
2019-01-23 10:09:14 -0800

    Added SpeedReader.

--- src/SpeedReader.java
+++ src/SpeedReader.java
@@ -1,7 +1,29 @@
 public class SpeedReader {
 
     public static void main(String[] args) {
-
+        while (true) {
+            In input = new In("speed.txt");
+            while (input.hasNextLine()) {
+                String line = input.readLine();
+                char[] characters = line.toCharArray();
+                int i = 0;
+                String word = "";
+                while (i < characters.length) {
+                    if (characters[i] == ' ') {
+                        StdDraw.clear();
+                        StdDraw.text(0.5, 0.5, word);
+                        StdDraw.pause(250);
+                        word = "";
+                    } else {
+                        word = word + characters[i];
+                    }
+                    i++;
+                }
+                StdDraw.clear();
+                StdDraw.text(0.5, 0.5, word);
+                StdDraw.pause(250);
+            }
+        }
     }
 
 }
