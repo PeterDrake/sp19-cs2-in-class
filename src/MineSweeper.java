12341a678e3515748c4ad31c46b3a4ae5006f7b5
Peter Drake <drake@lclark.edu>
2019-02-08 10:06:15 -0800

    We can now click to reveal squares.

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,13 +1,21 @@
 public class MineSweeper {
     public static void main(String[] args) {
-        boolean[][] grid = createGrid();
+      StdDraw.enableDoubleBuffering();
+       int gridWidth=8;
+        boolean[][] grid = createGrid(gridWidth);
+        boolean[][] revealed = new boolean[gridWidth][gridWidth];
+        revealed[2][3] = true;
         printGrid(grid);
         StdDraw.setScale(-0.5, grid.length - 0.5);
-        drawGrid(grid);
+        drawGrid(grid, revealed);
         while (true) {
             int[] click = getMouseClick(grid.length);
             StdOut.println(click[0]);
             StdOut.println(click[1]);
+            int r=click[0];
+            int c=click[1];
+            revealed[r][c]=true;
+            drawGrid(grid,revealed);
         }
     }
 
@@ -19,9 +27,9 @@ public class MineSweeper {
         double y = StdDraw.mouseY();
         while (StdDraw.isMousePressed()) {
         }
-        int r = (int)(Math.round(gridWidth-1-y));
-        int c = (int)(Math.round(x));
-        return new int[] {r,c};
+        int r = (int) (Math.round(gridWidth - 1 - y));
+        int c = (int) (Math.round(x));
+        return new int[]{r, c};
     }
 
     static int countNeighboringMines(int r, int c, boolean[][] grid) {
@@ -39,19 +47,27 @@ public class MineSweeper {
         return count;
     }
 
-    static void drawGrid(boolean[][] grid) {
+    static void drawGrid(boolean[][] grid, boolean[][] revealed) {
+        StdDraw.clear();
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid.length; c++) {
+                if (revealed[r][c]) {
+                    if (grid[r][c]) {
+                        StdDraw.filledCircle(c, grid.length - 1 - r, 0.25);
+                    }
+                    int n = countNeighboringMines(r, c, grid);
+                    if (n > 0) {
+                        StdDraw.text(c, grid.length - 1 - r, "" + n);
+                    }
+                } else {
+                    StdDraw.setPenColor(StdDraw.GRAY);
+                    StdDraw.filledSquare(c, grid.length - 1 - r, 0.5);
+                    StdDraw.setPenColor(StdDraw.BLACK);
+                }
                 StdDraw.square(c, grid.length - 1 - r, 0.5);
-                if (grid[r][c]) {
-                    StdDraw.filledCircle(c, grid.length - 1 - r, 0.25);
-                }
-                int n=countNeighboringMines(r,c,grid);
-                if (n>0) {
-                    StdDraw.text(c, grid.length - 1 - r, "" + n);
-                }
             }
         }
+        StdDraw.show();
     }
 
     static void printGrid(boolean[][] grid) {
@@ -67,8 +83,8 @@ public class MineSweeper {
         }
     }
 
-    static boolean[][] createGrid() {
-        boolean[][] grid = new boolean[8][8];
+    static boolean[][] createGrid(int w) {
+        boolean[][] grid = new boolean[w][w];
         for (int i = 0; i < 10; i++) {
             int row = StdRandom.uniform(grid.length);
             int column = StdRandom.uniform(grid.length);
@@ -79,5 +95,16 @@ public class MineSweeper {
             }
         }
         return grid;
+    }
+
+    static boolean allSafeSquaresRevealed(boolean[][] grid, boolean[][] revealed) {
+        for (int i = 0; i <grid.length; i++) {
+            for (int j = 0; j <grid.length ; j++) {
+            if(!grid[i][j]&&!revealed[i][j]){
+                return false;
+            }
+            }
+        }
+        return true;
     }
 }
--- test/MineSweeperTest.java
+++ test/MineSweeperTest.java
@@ -23,4 +23,19 @@ class MineSweeperTest {
         grid[2][2]=true;
         assertEquals(1,MineSweeper.countNeighboringMines(2,1,grid));
     }
+    @Test
+    void recognizesWin(){
+        boolean[][] grid = new boolean[3][3];
+        grid[0][1]=true;
+        grid[2][2]=true;
+        boolean[][] revealed=new boolean[3][3];
+        for(int i=0; i<3;i++){
+            for (int j = 0; j <3 ; j++) {
+                revealed[i][j]=true;
+            }
+        }
+        revealed[2][2]=false;
+        revealed[0][1]=false;
+        assertTrue(MineSweeper.allSafeSquaresRevealed(grid,revealed));
+    }
 }
