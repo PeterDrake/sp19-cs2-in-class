7c32e2d25cdbf68e4f5c9f896a565d270c9c6b36
drake <drake@lclark.edu>
2019-03-08 10:19:44 -0800

    Proper OO definition of Weapon.

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,7 +1,9 @@
+import java.util.Objects;
+
 public class Weapon {
-    String name;
-    String description;
-    int power;
+    private String name;
+    private String description;
+    private int power;
 
 
     Weapon(String n, String d, int p){
@@ -13,5 +15,41 @@ public class Weapon {
     int swing() {
         int roll = StdRandom.uniform(-1, 2);
         return this.power + roll;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    @Override
+    public String toString(){
+        return name + ", " + description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Weapon weapon = (Weapon) o;
+        return power == weapon.power &&
+                Objects.equals(name, weapon.name) &&
+                Objects.equals(description, weapon.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, power);
     }
 }
--- test/WeaponTest.java
+++ test/WeaponTest.java
@@ -6,18 +6,18 @@ class WeaponTest {
     @Test
     void remembersName(){
         Weapon w = new Weapon("sword", "iron, embedded with jewels", 5);
-        assertEquals("sword", w.name);
+        assertEquals("sword", w.getName());
     }
 
     @Test
     void remembersDescription(){
         Weapon w = new Weapon("sword", "iron, embedded with jewels", 5);
-        assertEquals("iron, embedded with jewels", w.description);
+        assertEquals("iron, embedded with jewels", w.getDescription());
     }
     @Test
     void remembersPower(){
         Weapon w = new Weapon("sword", "iron, embedded with jewels", 5);
-        assertEquals(5, w.power);
+        assertEquals(5, w.getPower());
     }
 
     @Test
@@ -34,6 +34,28 @@ class WeaponTest {
         Weapon a = new Weapon("axe", "steel", 1);
         int value = w.swing();
         assertTrue(value >= 4);
+    }
+
+    @Test
+    void equalsWorks(){
+        Weapon a = new Weapon("axe", "steel", 1);
+        Weapon b = new Weapon("axe", "steel", 1);
+        Weapon c = b;
+        assertEquals(a,b);
+        assertEquals(b,c);
+    }
+
+    @Test
+    void getterWorks(){
+        Weapon w = new Weapon("sword", "iron, embedded with jewels", 5);
+        assertEquals("sword", w.getName());
+    }
+
+    @Test
+    void setterWorks(){
+        Weapon w = new Weapon("sword", "iron, embedded with jewels", 5);
+        w.setName("excaliber");
+        assertEquals("excaliber",w.getName());
     }
 
 }
